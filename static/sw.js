0fca6c35036188ebcadfecf436a26ee5dbb01625
benamedd <benseti@yahoo.fr>
2025-03-29 21:31:12 +0100

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,23 +1,40 @@
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open("v1").then((cache) => {
-            return cache.addAll([
-                "./",
-                "./static/index.html",
-                "./static/styles.css",
-                "./static/script.js",
-                "./static/manifest.json",
-                "./static/icons/icon-192x192.png",
-                "./static/icons/icon-512x512.png"
-            ]);
-        })
-    );
+const CACHE_NAME = 'leafscan-v1';
+const STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/static/styles.css',
+  '/static/script.js',
+  '/static/manifest.json',
+  '/static/icons/icon-192x192.png',
+  '/static/icons/icon-512x512.png',
+  '/static/icons/favicon.ico'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(STATIC_FILES))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
         })
-    );
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
